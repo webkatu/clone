e59846abdff4c66dd173739fd7de8b617db6d8d2
shigure <mail@webkatu.com>
2015-07-07 00:54:00 +0900

    拡張可属性に対応。引数を増やした。

--- clone.js
+++ clone.js
@@ -1,8 +1,25 @@
+/*
+* オブジェクトをディープコピーするための関数;
+* 第一引数はコピーさせたいオブジェクトを渡す;
+* 第二引数はオブジェクトをどの程度同質にするかをオブジェクトで指定;
+* 例えば{descriptor: false, extensible: false}と指定すると
+* ディスクリプタはコピー元のオブジェクトと同じにならない(全てtrueになる)、
+* そして、オブジェクトの拡張可属性(frozen,sealedなど)は同じにならず、全て拡張可になる;
+* 指定しなければ全てコピー元のオブジェクトと同じになる;
+* 第三引数はコピーさせたくない型(親のprototype)を配列で渡す;
+* 第四引数はコピーさせたくないオブジェクトを配列で渡す;
+*/
+/*
+* 使い方;
+* clone(object, homogeneity, excludedPrototypes, excludedObjects);
+*/
 var clone = (function() {
+	//引数の型を返す関数;
 	var typeOf = function(operand) {
 		return Object.prototype.toString.call(operand).slice(8, -1);
 	};
 
+	//引数がプリミティブかオブジェクトか判定;
 	var isPrimitive = function(type) {
 		if(type === null) {
 			return true;
@@ -13,11 +30,82 @@ var clone = (function() {
 		return true;
 	};
 
+	//アクセサプロパティかデータプロパティか判定;
 	var isAccessorDescriptor = function(descriptor) {
 		return 'get' in descriptor;
 	};
 
-	var cloneNode = (function() {
+	//descriptorを同じにせず、get,set,value以外のdescriptor全てtrueのプロパティを定義;
+	var defineProperty = function(cloneObject, propName, descriptor, cloneParams) {
+		//cloneの引数が多すぎるのでbindする;
+		var boundClone = function(object) {
+			return clone(object, cloneParams.homogeneity, cloneParams.excludedPrototypes, cloneParams.excludedObjects, cloneParams.memo);
+		};
+
+		if(isAccessorDescriptor(descriptor)) {
+			//アクセサプロパティの場合;
+			Object.defineProperty(cloneObject, propName, {
+				get: boundClone(descriptor.get),
+				set: boundClone(descriptor.set),
+				enumerable: true,
+				configurable: true,
+			});
+		}else {
+			//データプロパティの場合;
+			Object.defineProperty(cloneObject, propName, {
+				value: boundClone(descriptor.value),
+				enumerable: true,
+				configurable: true,
+				writable: true,
+			});
+		}
+		return cloneObject;
+	};
+
+	//descriptorが同じプロパティを定義する;
+	var equalizeDescriptor = function(cloneObject, propName, descriptor, cloneParams) {
+		//cloneの引数が多すぎるのでbindする;
+		var boundClone = function(object) {
+			return clone(object, cloneParams.homogeneity, cloneParams.excludedPrototypes, cloneParams.excludedObjects, cloneParams.memo);
+		};
+
+		if(isAccessorDescriptor(descriptor)) {
+			//アクセサプロパティの場合;
+			Object.defineProperty(cloneObject, propName, {
+				get: boundClone(descriptor.get),
+				set: boundClone(descriptor.set),
+				enumerable: descriptor.enumerable,
+				configurable: descriptor.configurable,
+			});
+		}else {
+			//データプロパティの場合;
+			Object.defineProperty(cloneObject, propName, {
+				value: boundClone(descriptor.value),
+				enumerable: descriptor.enumerable,
+				configurable: descriptor.configurable,
+				writable: descriptor.writable,
+			});
+		}
+		return cloneObject;
+	};
+
+	//objectの拡張可属性を同じにする;
+	var equalizeExtensible = function(object, cloneObject) {
+		if(Object.isFrozen(object)) {
+			Object.freeze(cloneObject);
+			return;
+		}
+		if(Object.isSealed(object)) {
+			Object.seal(cloneObject);
+			return;
+		}
+		if(Object.isExtensible(object) === false) {
+			Object.preventExtensions(cloneObject);
+			return
+		}
+	};
+
+	var cloneNode = function(node) {
 
 		var cloneScript = function(element) {
 			var script = document.createElement('script');
@@ -48,10 +136,11 @@ var clone = (function() {
 		};
 
 		return cloneNode;
-	})();
+	}();
 
+	//型を作成するオブジェクト;
 	var sameTypeCreater = {
-		//objectの型と同一の型を返すメソッド;
+		//引数のobjectの型と同一の型を返すメソッド;
 		create: function(object) {
 			var type = typeOf(object);
 			var method = this[type];
@@ -99,7 +188,11 @@ var clone = (function() {
 		},
 	};
 
-	//memoオブジェクトを作る関数;
+	/*
+	* memoオブジェクトを作る関数;
+	* 一度コピーされたオブジェクトはmemoオブジェクトに保存され;
+	* 二度コピーすることがないようにする(循環参照対策);
+	*/
 	var createMemo = function() {
 		var memo = new Object();
 		var types = ['Object', 'Array', 'Function', 'Error', 'Date', 'RegExp', 'Boolean', 'String', 'Number'];
@@ -112,24 +205,28 @@ var clone = (function() {
 		return memo;
 	};
 
-	//第一引数はcloneしたいobject;
-	//第二引数はcloneしたくないobjectのconstructorのprototypeを配列で指定する;
-	//第三引数は循環参照対策のためのmemoオブジェクト;
-	function clone(object, prototypes, memo) {
+	//実際に呼ばれる関数;
+	//objectのプロパティを再帰的にコピーし、cloneObjectを返す;
+	function clone(object, homogeneity, excludedPrototypes, excludedObjects, memo) {
 		//プリミティブ型はそのまま返す;
 		if(isPrimitive(object)) {
 			return object;
 		}
-		//cloneしたくないobjectであれば、参照で返す;
-		if(prototypes.indexOf(Object.getPrototypeOf(object)) !== -1) {
+		//cloneしたくない型を持つobjectであれば、参照で返す;
+		if(excludedPrototypes.indexOf(Object.getPrototypeOf(object)) !== -1) {
 			return object;
 		}
+		//cloneしたくないobjectであれば、参照で返す;
+		if(excludedObjects.indexOf(object) !== -1) {
+			return object;
+		}
+
 		//Nodeオブジェクトは自作関数cloneNodeに処理を任せる;
 		if(object instanceof Node){
 			return cloneNode(object);
 		}
 
-		//objectの型とcloneObjectの型を同一にする;
+		//objectと同一の型を持つcloneObjectを作成する;
 		var cloneObject =  sameTypeCreater.create(object);
 		//cloneObjectがnullなら対応していないので参照で返す;
 		if(cloneObject === null) {
@@ -147,39 +244,54 @@ var clone = (function() {
 		memo[type]['objects'].push(object);
 		memo[type]['cloneObjects'].push(cloneObject);
 
-		//objectのすべてのプロパティを再帰的にcloneする;
-		//ディスクリプタも同一にする;
-		var properties = Object.getOwnPropertyNames(object);
-		properties.forEach(function(prop) {
-			var descriptor = Object.getOwnPropertyDescriptor(object, prop);
-			if(isAccessorDescriptor(descriptor)) {
-				//アクセサプロパティ;
-				Object.defineProperty(cloneObject, prop, {
-					get: clone(descriptor.get, prototypes, memo),
-					set: clone(descriptor.set, prototypes, memo),
-					enumerable: descriptor.enumerable,
-					configurable: descriptor.configurable,
-				});
-			}else {
-				//データプロパティ;
-				Object.defineProperty(cloneObject, prop, {
-					value: clone(descriptor.value, prototypes, memo),
-					enumerable: descriptor.enumerable,
-					configurable: descriptor.configurable,
-					writable: descriptor.writable,
-				});
+
+		var propNames = Object.getOwnPropertyNames(object);
+		var cloneParams = {
+			homogeneity: homogeneity,
+			excludedPrototypes: excludedPrototypes,
+			excludedObjects: excludedObjects,
+			memo: memo,
+		};
+		//objectのすべてのプロパティを再帰的にcloneして、cloneObjectのプロパティに加える;
+		propNames.forEach(function(propName) {
+			var descriptor = Object.getOwnPropertyDescriptor(object, propName);
+
+			if(propName in cloneObject) {
+				//オブジェクト生成時に自動的に定義されるネイティブプロパティ(lengthなど)なら
+				//ディスクリプタも同一にしてプロパティの内容をクローンする;
+				equalizeDescriptor(cloneObject, propName, descriptor, cloneParams);
+				return;
 			}
-			cloneObject[prop] = clone(object[prop], prototypes, memo);
+
+			//descriptorを全く同じにするか;
+			if(homogeneity.descriptor === false) {
+				//同じにしないならプロパティの内容だけクローンする;
+				defineProperty(cloneObject, propName, descriptor, cloneParams);
+			}else {
+				//ディスクリプタも同一にしてプロパティの内容をクローンする;
+				equalizeDescriptor(cloneObject, propName, descriptor, cloneParams);
+			}
 		});
 
-		//cloneしたオブジェクトを返す;
+		//objectの拡張可属性(preventExtensible, isSealed, isFrozen)を同一にするか;
+		if(homogeneity.extensible !== false) {
+			equalizeExtensible(object, cloneObject);
+		}
+
+		//クローンしたオブジェクトを返す;
 		return cloneObject;
 	}
 
-	return function(object, prototypes) {
-		if(! Array.isArray(prototypes)) {
-			prototypes = [];
+	return function(object, homogeneity, excludedPrototypes, excludedObjects) {
+		if(homogeneity === null || typeof homogeneity !== 'object') {
+			homogeneity = {};
 		}
-		return clone(object, prototypes, createMemo());
-	}
+		if(! Array.isArray(excludedPrototypes)) {
+			excludedPrototypes = [];
+		}
+		if(! Array.isArray(excludedObjects)) {
+			excludedObjects = [];
+		}
+		return clone(object, homogeneity, excludedPrototypes, excludedObjects, createMemo());
+	};
 })();
